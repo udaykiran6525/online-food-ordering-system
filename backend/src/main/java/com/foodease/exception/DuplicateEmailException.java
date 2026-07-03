4ac6cde6e44917684c0d0c86673ed400e41e6701
23NA1A5407 <242360739+udaykiran6525@users.noreply.github.com>
2026-07-03 19:42:03 +0530

    Delete backend directory

--- backend/src/main/java/com/foodease/exception/DuplicateEmailException.java
+++
@@ -1,7 +0,0 @@
-package com.foodease.exception;
-
-public class DuplicateEmailException extends RuntimeException {
-    public DuplicateEmailException(String email) {
-        super("Email already registered: " + email + ". Please login instead.");
-    }
-}
