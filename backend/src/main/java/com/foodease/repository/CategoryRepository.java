cb06099171872cfaadc325a26a0a3a9a2f5bc699
23NA1A5407 <242360739+udaykiran6525@users.noreply.github.com>
2026-07-04 16:13:50 +0530

    Update CategoryRepository.java

--- backend/src/main/java/com/foodease/repository/CategoryRepository.java
+++ backend/src/main/java/com/foodease/repository/CategoryRepository.java
@@ -9,5 +9,4 @@ import java.util.List;
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     List<Category> findByRestaurantId(Long restaurantId);
-    List<Category> findByRestaurantIsNull();
 }
