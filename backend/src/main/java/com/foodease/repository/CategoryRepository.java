519983df49504a17d3c2e9a495d9b5fd2fc885d2
23NA1A5407 <242360739+udaykiran6525@users.noreply.github.com>
2026-07-04 17:37:53 +0530

    Rename method to findByRestaurantId and add findByRestaurantIsNull

--- backend/src/main/java/com/foodease/repository/CategoryRepository.java
+++ backend/src/main/java/com/foodease/repository/CategoryRepository.java
@@ -8,5 +8,6 @@ import java.util.List;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
-    List<Category> findByRestaurant_Id(Long restaurantId);
+    List<Category> findByRestaurantId(Long restaurantId);
+    List<Category> findByRestaurantIsNull();
 }
